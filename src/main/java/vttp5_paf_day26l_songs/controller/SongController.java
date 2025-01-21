10fa0df00637802936ed200c05d7e5d00d6c1c3a
Jo Yie <joyieleong@gmail.com>
2025-01-21 15:05:41 +0800

    removed unneeded code

--- src/main/java/vttp5_paf_day26l_songs/controller/SongController.java
+++ src/main/java/vttp5_paf_day26l_songs/controller/SongController.java
@@ -34,49 +34,6 @@ public class SongController {
 
     }
 
-    @GetMapping("/test")
-    public String test() {
-
-        List<Document> results = songRepo.getAllSongsByYear(2000);
-
-        for (Document d: results) {
-            System.out.println(d.get("track_name")); 
-            System.out.println(d.get("artist(s)_name")); 
-
-            // System.out.println(d);
-        }
-
-        return "test";
-
-    }
-
-    // @GetMapping("/year/{year}")
-    // public String getAllSongsFromYear(@PathVariable Integer year, Model model) { 
-
-    //     List<Document> results = songRepo.getAllSongsByYear(year);
-    //     List<SongObject> songObjects = new ArrayList<>(); 
-
-    //     for (Document d: results) {
-
-    //         SongObject so = new SongObject(); 
-
-    //         String trackName = d.get("track_name").toString();
-    //         String artistName = d.get("artist(s)_name").toString();
-
-    //         so.setTrackName(trackName);
-    //         so.setArtistName(artistName);
-
-    //         songObjects.add(so);
-
-    //     }
-
-    //     model.addAttribute("year", year);
-    //     model.addAttribute("songObjects", songObjects);
-
-    //     return "songs";
-
-    // }
-
     @GetMapping("/songs")
     public String getAllSongsFromYear(@RequestParam("year") Integer year, Model model) { 
 
